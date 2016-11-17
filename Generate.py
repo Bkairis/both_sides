183a2d643859761d4d7b0a29b00b2231b0faa193
jamesjbrownjr <jim.brown@rutgers.edu>
2016-11-16 20:23:58 -0500

    Add files via upload

--- Generate.py
+++ Generate.py
@@ -1,19 +1,17 @@
 
 # coding: utf-8
 
-# In[1]:
+# my comments are more important--i mean it this time
 
-def trumpSentence(text):  #trumpsentence
+def trumpSentence(text):
     import markovify
     with open(text) as f:
         text = f.read()
     text_model = markovify.Text(text)
     return(text_model.make_sentence())
 
-save me
 
 
-# In[ ]:
 
 
 
